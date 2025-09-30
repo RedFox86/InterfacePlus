c4f331a146f75c09bbaaea00085de2e5b818e4ba
Peter <85247098+RedFox86@users.noreply.github.com>
2025-09-30 16:41:42 -0400

    Remove logging from Renderer

--- src/main/java/net/redfox/interfaceplus/object/Renderer.java
+++ src/main/java/net/redfox/interfaceplus/object/Renderer.java
@@ -1,23 +1,16 @@
 package net.redfox.interfaceplus.object;
 
 import java.util.ArrayList;
-import java.util.logging.Logger;
 
 import net.redfox.interfaceplus.gui.util.WindowContext;
 
 public class Renderer {
     private final ArrayList<Renderable> objects;
     private final ArrayList<Renderable> renderableObjectQueue;
-    private final Logger logger;
 
     public Renderer() {
         objects = new ArrayList<>();
         renderableObjectQueue = new ArrayList<>();
-        logger = Logger.getLogger("Renderer");
-    }
-
-    public Logger getLogger() {
-        return logger;
     }
 
     public void renderObjects(WindowContext context) {
