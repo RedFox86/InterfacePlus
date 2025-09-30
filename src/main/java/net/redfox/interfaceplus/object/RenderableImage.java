c9c0e8b545d66ef7e7916abb30a582aed2346fd2
Peter <85247098+RedFox86@users.noreply.github.com>
2025-09-30 16:41:10 -0400

    Formatting

--- src/main/java/net/redfox/interfaceplus/object/RenderableImage.java
+++ src/main/java/net/redfox/interfaceplus/object/RenderableImage.java
@@ -6,8 +6,8 @@ import net.redfox.interfaceplus.math.Vector2;
 
 import java.awt.image.BufferedImage;
 
-public abstract class RenderableImage extends RenderableObject{
 @SuppressWarnings("unused")
+public abstract class RenderableImage extends RenderableObject {
   protected BufferedImage displayImage;
   protected RenderableImage(BufferedImage displayImage, Vector2 position) {
     super(new Size2(displayImage.getWidth(), displayImage.getHeight()), position);
