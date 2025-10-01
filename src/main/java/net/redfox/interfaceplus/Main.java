dd54c1bc7feae36e94386bab14523cd2c402f5fc
Peter <85247098+RedFox86@users.noreply.github.com>
2025-10-01 18:17:06 -0400

    Update main method

--- src/main/java/net/redfox/interfaceplus/Main.java
+++ src/main/java/net/redfox/interfaceplus/Main.java
@@ -5,7 +5,7 @@ import static java.awt.Color.*;
 import net.redfox.interfaceplus.gui.display.Interface;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main() {
         Interface i = new Interface.Builder().title("Hello").color(WHITE).build();
     }
 }
